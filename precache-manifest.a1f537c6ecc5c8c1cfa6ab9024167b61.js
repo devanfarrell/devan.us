f40c7d7cb789e6544db977ec96fc8e2475d65630
Devan Farrell <devan.farrell16@gmail.com>
2019-09-01 10:06:45 -0700

    Updates

---
+++ precache-manifest.a1f537c6ecc5c8c1cfa6ab9024167b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc4f79d9ab289c0bc19eceb220341f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13ec1b985eedfc11c63d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "99235f681e37e2966d4e",
+    "url": "/static/js/2.bb52b52a.chunk.js"
+  },
+  {
+    "revision": "13ec1b985eedfc11c63d",
+    "url": "/static/js/main.64cbd0df.chunk.js"
+  },
+  {
+    "revision": "f0318d415a9673e521d8",
+    "url": "/static/js/runtime~main.33cfb048.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
